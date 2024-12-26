b9cf2c9735c8ace8bbd675e01dda56b5077eb326
Lihao006 <lihao.jin@estudiantat.upc.edu>
2024-12-26 17:42:51 +0100

    Update SolucioLihaoMarc.pl

--- SolucioLihaoMarc.pl
+++ SolucioLihaoMarc.pl
@@ -1,7 +1,32 @@
+% Bibliografia:
+% https://helloauto.com/es-es/glosario/
+
+
+
+
 subsistema(cotxe, direccio).
 subsistema(cotxe, transmissio).
 subsistema(cotxe, encesa).
 subsistema(cotxe, sistema_electric).
 subsistema(cotxe, sistema_de_confort_interior).
 
-avaria(direccio, cinturo_desgastat).
+% Avaries cotxes
+avaria(direccio, manegues_desgastades).
+avaria(direccio, baix_nivell_oli_direccio).
+avaria(direccio, fallada_sistema_hidraulic).
+
+avaria(transmissio, baix_nivell_flux).
+avaria(transmissio, embragatge_desgastat).
+avaria(transmissio, fallada_sistema_hidraulic).
+
+avaria(encesa, bateria_defectuosa).
+avaria(encesa, fallada_bobina_encendre).
+avaria(encesa, fusibles_cremats).
+
+avaria(sistema_electric, bateria_defectuosa).
+avaria(sistema_electric, alternador_defectuos).
+avaria(sistema_electric, fusibles_cremats).
+
+avaria(sistema_de_confort_interior, fallada_motor_ventilador).
+avaria(sistema_de_confort_interior, fuga_gas_refrigerant).
+ava
