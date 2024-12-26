52741bea2ca23dcd10ffc9c5c25a818bca982c60
Lihao006 <lihao.jin@estudiantat.upc.edu>
2024-12-26 17:08:38 +0100

    Update SolucioLihaoMarc.pl

--- SolucioLihaoMarc.pl
+++ SolucioLihaoMarc.pl
@@ -2,3 +2,6 @@ subsistema(cotxe, direccio).
 subsistema(cotxe, transmissio).
 subsistema(cotxe, encesa).
 subsistema(cotxe, sistema_electric).
+subsistema(cotxe, sistema_de_confort_interior).
+
+avaria(direccio, cinturo_desgastat).
