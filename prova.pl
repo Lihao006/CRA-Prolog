ac93fca1429badc6378ace839776cda0952e0b75
MSS136 <marc.sanchez.sala@estudiantat.upc.edu>
2024-12-28 17:11:22 +0100

    Update prova.pl

--- prova.pl
+++ prova.pl
@@ -134,7 +134,7 @@ printllista([(Subsistema, Causa, _)|Altres]) :-
 
 % Funció per preguntar a usuari si les causes filtrades que queden son correctes
 % Suposem que l usuari va a comprovar si els components dels subsistemes esmentades estan correctes o no
-pregunta_usuari([]).
+pregunta_usuari([]) :- format("No s'ha pogut trobar l'avaria.~n").
 pregunta_usuari([(Subsistema, Causa, _)|Altres]) :-
     format("Comprova si hi ha ~w en el subsistema ~w? (si/no): ", [Causa, Subsistema]), nl,
     read(Resposta),
