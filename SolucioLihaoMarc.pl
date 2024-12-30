785bacb4639f4fc482567d290783e84a966b7026
MSS136 <marc.sanchez.sala@estudiantat.upc.edu>
2024-12-30 18:34:17 +0100

    Update SolucioLihaoMarc.pl

--- SolucioLihaoMarc.pl
+++ SolucioLihaoMarc.pl
@@ -4,97 +4,97 @@ sistema(cuina).
 
 
 % Subsistemes de cotxe
-subsistema(cotxe, direccio).
-subsistema(cotxe, transmissio).
-subsistema(cotxe, encesa).
-subsistema(cotxe, electric).
-subsistema(cotxe, confort_interior).
-subsistema(cotxe, motor).
+subsistema(cotxe, cotxe_direccio).
+subsistema(cotxe, cotxe_transmissio).
+subsistema(cotxe, cotxe_encesa).
+subsistema(cotxe, cotxe_electric).
+subsistema(cotxe, cotxe_confort_interior).
+subsistema(cotxe, cotxe_motor).
 
 % Subsistemes de refrigerador
-subsistema(refrigerador, refrigerant).
-subsistema(refrigerador, electric).
-subsistema(refrigerador, aillament).
-subsistema(refrigerador, descongelacio).
+subsistema(refrigerador, refrigerador_refrigerant).
+subsistema(refrigerador, refrigerador_electric).
+subsistema(refrigerador, refrigerador_aillament).
+subsistema(refrigerador, refrigerador_descongelacio).
 
 % Subsistemes de cuina
-subsistema(cuina, electric).
-subsistema(cuina, gas).
-subsistema(cuina, mecanic).
+subsistema(cuina, cuina_electric).
+subsistema(cuina, cuina_gas).
+subsistema(cuina, cuina_mecanic).
 
 
 % Avaries cotxes
-avaria(direccio, [no_es_mou, vibracions], manegues_desgastades).
-avaria(direccio, [no_es_mou], baix_nivell_oli).
-avaria(direccio, [no_es_mou], fallada_sistema_hidraulic).
-avaria(direccio, [vibracions], pneumatic_punxat).
+avaria(cotxe_direccio, [no_es_mou, vibracions], manegues_desgastades).
+avaria(cotxe_direccio, [no_es_mou], baix_nivell_oli).
+avaria(cotxe_direccio, [no_es_mou], fallada_sistema_hidraulic).
+avaria(cotxe_direccio, [vibracions], pneumatic_punxat).
 
-avaria(transmissio, [no_es_mou], baix_nivell_liquid_refrigerant).
-avaria(transmissio, [no_es_mou], embragatge_desgastat).
-avaria(transmissio, [no_es_mou], fallada_sistema_hidraulic).
-avaria(transmissio, [vibracions], eixos_desgastats).
-avaria(transmissio, [vibracions], amortidors_desgastats).
-avaria(transmissio, [vibracions], caixa_canvis_defectuosa).
+avaria(cotxe_transmissio, [no_es_mou], baix_nivell_liquid_refrigerant).
+avaria(cotxe_transmissio, [no_es_mou], embragatge_desgastat).
+avaria(cotxe_transmissio, [no_es_mou], fallada_sistema_hidraulic).
+avaria(cotxe_transmissio, [vibracions], eixos_desgastats).
+avaria(cotxe_transmissio, [vibracions], amortidors_desgastats).
+avaria(cotxe_transmissio, [vibracions], caixa_canvis_defectuosa).
 
-avaria(encesa, [no_encen_motor, no_es_mou], bateria_descarregada).
-avaria(encesa, [no_encen_motor, no_es_mou], fallada_bobina_encendre).
-avaria(encesa, [no_encen_motor, no_es_mou], fusibles_cremats).
-avaria(encesa, [no_encen_motor, no_es_mou], falta_combustible).
+avaria(cotxe_encesa, [no_encen_motor, no_es_mou], bateria_descarregada).
+avaria(cotxe_encesa, [no_encen_motor, no_es_mou], fallada_bobina_encendre).
+avaria(cotxe_encesa, [no_encen_motor, no_es_mou], fusibles_cremats).
+avaria(cotxe_encesa, [no_encen_motor, no_es_mou], falta_combustible).
 
-avaria(electric, [no_encen_motor, no_es_mou, fars_no_funcionen], bateria_descarregada).
-avaria(electric, [fars_no_funcionen], alternador_defectuos).
-avaria(electric, [no_encen_motor, no_es_mou, fars_no_funcionen], fusibles_cremats).
+avaria(cotxe_electric, [no_encen_motor, no_es_mou, fars_no_funcionen], bateria_descarregada).
+avaria(cotxe_electric, [fars_no_funcionen], alternador_defectuos).
+avaria(cotxe_electric, [no_encen_motor, no_es_mou, fars_no_funcionen], fusibles_cremats).
 
-avaria(confort_interior, [no_funciona_aire_acondicionat], fallada_motor_ventilador).
-avaria(confort_interior, [no_funciona_aire_acondicionat], fuga_gas_refrigerant).
-avaria(confort_interior, [no_funciona_aire_acondicionat], fallada_compressor).
-avaria(confort_interior, [no_funciona_aire_acondicionat], filtre_aire_defectuos).
+avaria(cotxe_confort_interior, [no_funciona_aire_acondicionat], fallada_motor_ventilador).
+avaria(cotxe_confort_interior, [no_funciona_aire_acondicionat], fuga_gas_refrigerant).
+avaria(cotxe_confort_interior, [no_funciona_aire_acondicionat], fallada_compressor).
+avaria(cotxe_confort_interior, [no_funciona_aire_acondicionat], filtre_aire_defectuos).
 
-avaria(motor, [no_encen_motor, no_es_mou], bugia_defectuosa).
-avaria(motor, [no_encen_motor, no_es_mou], bomba_aigua_defectuosa).
-avaria(motor, [no_es_mou], carter_danyat).
-avaria(motor, [vibracions], filtres_motor_defectuosos).
-avaria(motor, [vibracions], baix_nivell_oli).
+avaria(cotxe_motor, [no_encen_motor, no_es_mou], bugia_defectuosa).
+avaria(cotxe_motor, [no_encen_motor, no_es_mou], bomba_aigua_defectuosa).
+avaria(cotxe_motor, [no_es_mou], carter_danyat).
+avaria(cotxe_motor, [vibracions], filtres_motor_defectuosos).
+avaria(cotxe_motor, [vibracions], baix_nivell_oli).
 
-avaria(fre, [no_frena, vibracions], pastilles_desgastades).
-avaria(fre, [no_frena, vibracions], discos_desgastats).
-avaria(fre, [no_frena], bomba_frens_defectuosa).
-avaria(fre, [no_frena], problema_abs).
+avaria(cotxe_fre, [no_frena, vibracions], pastilles_desgastades).
+avaria(cotxe_fre, [no_frena, vibracions], discos_desgastats).
+avaria(cotxe_fre, [no_frena], bomba_frens_defectuosa).
+avaria(cotxe_fre, [no_frena], problema_abs).
 
 
 % Avaries refrigeradors
-avaria(refrigerant, [no_refreda, soroll_fort, fuita_gas], compressor_defectuos).
-avaria(refrigerant, [no_refreda, fuita_gas, fuita_liquid], condensador_defectuos).
-avaria(refrigerant, [no_refreda, fuita_gas, fuita_liquid], evaporador_defectuos).
-avaria(refrigerant, [no_refreda], valvula_expansio_defectuos).
+avaria(refrigerador_refrigerant, [no_refreda, soroll_fort, fuita_gas], compressor_defectuos).
+avaria(refrigerador_refrigerant, [no_refreda, fuita_gas, fuita_liquid], condensador_defectuos).
+avaria(refrigerador_refrigerant, [no_refreda, fuita_gas, fuita_liquid], evaporador_defectuos).
+avaria(refrigerador_refrigerant, [no_refreda], valvula_expansio_defectuos).
 
-avaria(electric, [marca_malament_temperatura, no_encen_panell], panell_defectuos).
-avaria(electric, [marca_malament_temperatura, no_refreda, refreda_massa], termostat_defectuos).
-avaria(electric, [no_encen_llum], bombeta_fossa).
-avaria(electric, [no_refreda], ventilador_defectuos).
-avaria(electric, [no_refreda, no_encen_llum, no_encen_panell], falta_potencia_electrica).
+avaria(refrigerador_electric, [marca_malament_temperatura, no_encen_panell], panell_defectuos).
+avaria(refrigerador_electric, [marca_malament_temperatura, no_refreda, refreda_massa], termostat_defectuos).
+avaria(refrigerador_electric, [no_encen_llum], bombeta_fossa).
+avaria(refrigerador_electric, [no_refreda], ventilador_defectuos).
+avaria(refrigerador_electric, [no_refreda, no_encen_llum, no_encen_panell], falta_potencia_electrica).
 
-avaria(aillament, [no_refreda], aillament_termic_defectuos).
-avaria(aillament, [no_tanca_be], juntes_porta_defectuoses).
+avaria(refrigerador_aillament, [no_refreda], aillament_termic_defectuos).
+avaria(refrigerador_aillament, [no_tanca_be], juntes_porta_defectuoses).
 
-avaria(descongelacio, [acumulacio_gel], escalfador_defectuos).
-avaria(descongelacio, [surt_aigua], drenatge_defectuos).
+avaria(refrigerador_descongelacio, [acumulacio_gel], escalfador_defectuos).
+avaria(refrigerador_descongelacio, [surt_aigua], drenatge_defectuos).
 
 
 
 % Avaries cuines
-avaria(electric, [acumulacio_fum], extractor_defectuos).
-avaria(electric, [no_encen_panell_forn, forn_no_calenta], panell_forn_defectuos).
-avaria(electric, [forn_no_calenta, salta_diferencial], resistencia_defectuos).
-avaria(electric, [forn_no_calenta], falta_potencia_electrica).
-avaria(electric, [forn_no_calenta, no_encen_panell_forn], font_alimentacio_defectuosa).
+avaria(cuina_electric, [acumulacio_fum], extractor_defectuos).
+avaria(cuina_electric, [no_encen_panell_forn, forn_no_calenta], panell_forn_defectuos).
+avaria(cuina_electric, [forn_no_calenta, salta_diferencial], resistencia_defectuos).
+avaria(cuina_electric, [forn_no_calenta], falta_potencia_electrica).
+avaria(cuina_electric, [forn_no_calenta, no_encen_panell_forn], font_alimentacio_defectuosa).
 
-avaria(gas, [no_encenen_fogons, pudor_gas, escoltar_xiulet], fuita_gas).
-avaria(gas, [no_encenen_fogons], falta_gas).
-avaria(gas, [no_encenen_fogons, flama_dispersa], fogons_taponats).
-avaria(gas, [flama_dispersa], xiclets_fogons_defectuosos).
+avaria(cuina_gas, [no_encenen_fogons, pudor_gas, escoltar_xiulet], fuita_gas).
+avaria(cuina_gas, [no_encenen_fogons], falta_gas).
+avaria(cuina_gas, [no_encenen_fogons, flama_dispersa], fogons_taponats).
+avaria(cuina_gas, [flama_dispersa], xiclets_fogons_defectuosos).
 
-avaria(mecanic, [no_encenen_fogons, no_regulen_fogons_be], panell_fogons_defectuos).
+avaria(cuina_mecanic, [no_encenen_fogons, no_regulen_fogons_be], panell_fogons_defectuos).
 
 
 
